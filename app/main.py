7038d46840f59838168ab5da34fbc3d2ebb5cbaf
Olie <olie@obsoletelabs.net>
2025-12-22 22:21:07 +0800

    bettter ip logging to see error

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 logger.info("Service starting up...")
 
 
-old_ip = None # initialize old_ip variable
+old_ip = "0.0.0.0" # initialize old_ip variable
 
 try:
     sleep_time = int(os.environ.get("INTERVAL_SECONDS")) # check interval from environment variable
@@ -50,22 +50,29 @@ def main():
         #API_TOKEN = os.environ.get("API_TOKEN")
         #FILE_PATH = Path("/ip.txt") # location to cache ip addresses
 
-        found, ip = get_ip(WHOAMI_URLS=WHOAMI_URLS) # grab the current ip address
+        found, current_ip = get_ip(WHOAMI_URLS=WHOAMI_URLS) # grab the current ip address
         #print(f"Current IP: {ip}")
         if found:
-            logger.info(f"Current IP: {ip}")
+            logger.info(f"Current IP: {current_ip}")
         else:
             logger.warning("Could not retrieve current IP address.")
 
         try:
-            if found and ip != old_ip: # if ip has changed
-                new_ip = ip
-                #print(f"IP change detected: {old_ip} --> {new_ip}")
-                logger.info(f"IP change detected: {old_ip} --> {new_ip}")
-                update_ip.cloudflare(API_TOKEN, old_ip, new_ip)
-                old_ip = ip
+            logger.info(f"Old IP: {old_ip}")
+            if found and current_ip != old_ip: # if ip has changed
+
+                logger.info(f"IP change detected: {old_ip} --> {current_ip}")
+                try:
+                    logger.info("Updating IP address via Cloudflare API...")
+                    #print("Updating IP address via Cloudflare API...")
+                    update_ip.cloudflare(API_TOKEN, old_ip, current_ip)
+                except Exception as e:
+                    logger.error(f"Error updating IP address via Cloudflare API: {e}")
+                    #print(f"Error updating IP address via Cloudflare API: {e}")
+
+                old_ip = current_ip
                 #print(f"Updated cached IP to: {new_ip}")
-                logger.info(f"Updated IP address to: {new_ip}")
+                logger.info(f"Updated IP address to: {current_ip}")
         except:
             #print("Error updating IP address.")
             logger.error("Error updating IP address.")
