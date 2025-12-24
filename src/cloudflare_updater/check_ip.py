ead45df1e158cb13523039183cd7ecf16026b3d8
Olie <olie@obsoletelabs.net>
2025-12-24 16:41:10 +0800

    change to failover ip logging --> failover logs are now warning level, and state failover.

--- src/cloudflare_updater/check_ip.py
+++ src/cloudflare_updater/check_ip.py
@@ -10,10 +10,15 @@ logger = logging.getLogger(__name__)
 
 def get_ip(whoami_urls: list[str]) -> str | None:
     """takes a list of urls to whoami websites and uses them to find the current ip address"""
+    firsttry = True
 
     for url in whoami_urls:  # Attempts to use each url fails over to the next one
         try:
-            logger.info("Checking IP via %s", url)
+            if firsttry:
+                firsttry = False
+                logger.info("Checking IP via %s", url)
+            else:
+                logger.warning("Failover: Checking IP via %s", url)
             result = requests.get(url, timeout=3)
             result.raise_for_status()
             text = result.text.strip()  # clean up the text
