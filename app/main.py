3862645da83c7dcf27f8d10324b1da2cac6cbfa2
Hotzings <hotzings@gmail.com>
2025-12-23 23:51:37 -0500

    Colored logging

--- app/main.py
+++ app/main.py
@@ -6,16 +6,18 @@ import os
 import logging
 import sys
 
-import requests
+from colorlog import ColoredFormatter
 
 import update_ip
 from check_ip import get_ip
 
 from utilities.send_webhooks import send as send_webhooks
+from utilities import env_loaders
 
 ################################
 #           LOGGING            #
 ################################
+DEBUG_LOGGER_FORMAT = False # should be disabled for production
 
 # Set up logging, default to INFO level
 LOGGING_LEVEL = os.environ.get("LOG_LEVEL", "INFO").strip().upper()
@@ -32,79 +34,101 @@ else:
     LOG_LEVEL = logging.INFO
     LOGGING_LEVEL = "INFO"
 
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] %(message)s",
-    handlers=[logging.StreamHandler(sys.stdout)]
+
+# logger setup
+
+formatter = ColoredFormatter(
+    "%(log_color)s%(asctime)s [%(levelname)s]%(reset)s %(message_log_color)s%(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    log_colors={
+        "DEBUG":    "light_black",        # color for the prefix
+        "INFO":     "reset",
+        "WARNING":  "yellow",
+        "ERROR":    "red",
+        "CRITICAL": "bold_red",
+    },
+    secondary_log_colors={
+        "message": {
+            "DEBUG": "light_black",   # <-- gray message text
+            "INFO": "reset",
+            "WARNING": "yellow",
+            "ERROR": "yellow",
+            "CRITICAL": "yellow",
+        }
+    }
 )
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    handlers=[handler]
+)
+
 logger = logging.getLogger(__name__)
 
+# for testing formats
+if DEBUG_LOGGER_FORMAT:
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warning")
+    logger.error("error")
+    logger.critical("critical")
+
+
+
+# finish up
 logger.info("Logging level set to %s", LOGGING_LEVEL) # log the logging level as critical to ensure logged.
 logger.setLevel(LOG_LEVEL)
 
 logger.info("Service starting up...") # log startup
 
+
+
+
+
+
+logger.info("""
 ################################
 #          LOAD ENV            #
 ################################
+""")
 
 # Get sleep time from environment variable or use default
 sleep_time = int(os.environ.get("CHECK_INTERVAL_SECONDS", 600))
 logger.info("Check interval set to %s seconds.", sleep_time)
+
 
 # Get retry interval from environment variable or use default
 retry_interval = int(os.environ.get("RETRY_INTERVAL_SECONDS", 10)) # retry interval from environment variable
 logger.info("Retry interval set to %s seconds.", retry_interval)
 
 
-# Get API token from environment variable, check if valid
-CLOUDFLARE_API_TOKEN = os.environ.get("CLOUDFLARE_API_TOKEN", None)
-if CLOUDFLARE_API_TOKEN:
-    r = requests.get("https://api.cloudflare.com/client/v4/user/tokens/verify", timeout=3, headers={"Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"}).json()
-
-    if r.get("success") is False: # check if api key is invalid
-        logger.error("Cloudflare API token is invalid, please double check and try again. Exiting.")
-        logger.error(r.get("errors"))
-        exit(1)
-else:
-    logger.error("CLOUDFLARE_API_TOKEN environment variable not set. Exiting.")
-    exit(1)
-logger.info("Cloudflare API token validated.")
-
-
-# Get WHOAMI_URLS from environment variable or use default
-WHOAMI_URLS = os.environ.get("WHOAMI_URLS", "").split(",")
-if os.environ.get("OVERRIDE_OBSOLETE_WHOAMI", "false").lower() == "false":
-    WHOAMI_URLS.append("http://whoami.obsoletelabs.org:12345/") # default fallback
-else:
-    logger.info("Obsolete WHOAMI URL NOT appended due to OVERRIDE_OBSOLETE_WHOAMI setting.")
-
-if WHOAMI_URLS == [""]:
-    logger.error("WHOAMI_URLS environment variable empty and obsolete fallback disabled. Exiting.")
+# Get cloudflare api token
+success, CLOUDFLARE_API_TOKEN = env_loaders.get_cloudflare_api_token()
+if not success:
     exit(1)
 
-# Validate WHOAMI_URLS
-for URL in WHOAMI_URLS:
-    if URL == "":
-        WHOAMI_URLS.remove(URL)
-    elif not URL.startswith("http://") and not URL.startswith("https://"):
-        logger.warning("Invalid WHOAMI_URL detected and removed: %s", URL)
-        WHOAMI_URLS.remove(URL)
-
+# Get whoami urls
+success, WHOAMI_URLS = env_loaders.get_whoami_urls()
+if not success:
+    exit(1)
 logger.info("Using WHOAMI_URLS: %s", WHOAMI_URLS)
 
 
 # Get the initial IP address, log, and set as OLD_IP
-initial_ip = os.environ.get("INITIAL_IP", get_ip(whoami_urls=WHOAMI_URLS)[1])
+initial_ip = os.environ.get("INITIAL_IP", None)
+if initial_ip:
+    logger.warning("Initial IP overwritten by debug value (Not recemended for production use)")
+else:
+    initial_ip = get_ip(whoami_urls=WHOAMI_URLS)[1]
 logger.info("Initial IP set to: %s", initial_ip)
 OLD_IP = initial_ip
-
 
 ################################
 #           Notify             #
 ################################
-
-#EXTERNAL_NOTIFIERS = False
 SMTP_NOTIFIER_ENABLED = os.environ.get("SMTP_NOTIFIER_ENABLED", "false").lower() == "true"
 
 def notify_ip_change(old_ip, new_ip):
@@ -165,5 +189,9 @@ def main():
 
 # Run main function
 if __name__ == "__main__":
-    logger.info("Service running") # log running
+    logger.info("""
+################################
+#      Service running         #
+################################
+""")
     main()
--- app/utilities/env_loaders.py
+++ app/utilities/env_loaders.py
@@ -1,5 +1,48 @@
 """Code to import environment variables and verify them"""
 
 import logging
+from os import environ
+import requests
 
 logger = logging.getLogger(__name__)
+
+
+def get_cloudflare_api_token():
+    """Get API token from environment variable, check if valid"""
+
+    CLOUDFLARE_API_TOKEN = environ.get("CLOUDFLARE_API_TOKEN", None)
+    if CLOUDFLARE_API_TOKEN:
+        r = requests.get("https://api.cloudflare.com/client/v4/user/tokens/verify", timeout=3, headers={"Authorization": f"Bearer {CLOUDFLARE_API_TOKEN}"}).json()
+
+        if r.get("success") is False: # check if api key is invalid
+            logger.error("Cloudflare API token is invalid, please double check and try again. Exiting.")
+            logger.error(r.get("errors"))
+            return(False, None)
+        return (True, CLOUDFLARE_API_TOKEN)
+    else:
+        logger.error("CLOUDFLARE_API_TOKEN environment variable not set. Exiting.")
+        return(False, None)
+    logger.info("Cloudflare API token validated.")
+
+def get_whoami_urls():
+    """Get WHOAMI_URLS from environment variable or use default"""
+
+    WHOAMI_URLS = environ.get("WHOAMI_URLS", "").split(",")
+    if environ.get("OVERRIDE_OBSOLETE_WHOAMI", "false").lower() == "false":
+        WHOAMI_URLS.append("http://whoami.obsoletelabs.org:12345/") # default fallback
+    else:
+        logger.info("Obsolete WHOAMI URL NOT appended due to OVERRIDE_OBSOLETE_WHOAMI setting.")
+
+    if WHOAMI_URLS == [""]:
+        logger.error("WHOAMI_URLS environment variable empty and obsolete fallback disabled. Exiting.")
+        return(False, None)
+
+    # Validate WHOAMI_URLS
+    for URL in WHOAMI_URLS:
+        if URL == "":
+            WHOAMI_URLS.remove(URL)
+        elif not URL.startswith("http://") and not URL.startswith("https://"):
+            logger.warning("Invalid WHOAMI_URL detected and removed: %s", URL)
+            WHOAMI_URLS.remove(URL)
+
+    return(True, WHOAMI_URLS)
